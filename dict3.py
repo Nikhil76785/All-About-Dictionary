150e633064cffa2794be06bbc68ca7008cbd32c8
Nikhil76785 <valivetinikhil2@gmail.com>
2025-08-20 20:05:53 +0530

    COUNTRY CODES

---
+++ dict3.py
@@ -0,0 +1,6 @@
+country_code = {
+    'india': '0091', 'australia': '0061', 'nepal': '0977'
+}
+
+print(f"Country code of India: {country_code.get('india', 'Not Found')}")
+print(f"Country code of Japan: {country_code.get('japan', 'Not Found')}")
